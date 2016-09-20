060281d5fe59ea9a2858c64889b7815977c4d6a6
Rishal Hurbans <rishal.hurbans@gmail.com>
2016-09-20 16:49:14 +0200

    Added image processing code, dataset, and README.

---
+++ 4-Image Processing/coins.py
@@ -0,0 +1,40 @@
+import numpy as np
+from skimage import data, io, filters
+from skimage.feature import canny
+from scipy import ndimage as ndi
+from skimage.filters import sobel
+from skimage.morphology import watershed
+
+# Load coins image data
+image = data.coins()
+io.imshow(image)
+io.show()
+
+# Find edges with the Sobel algorithm
+sobelEdges = filters.sobel(image)
+io.imshow(sobelEdges)
+io.show()
+
+# Find edges with the Canny algorithm
+cannyEdges = edges = canny(image/255.)
+io.imshow(cannyEdges)
+io.show()
+
+# Fill holes in the edges found with the Canny algorithm
+fill_coins = ndi.binary_fill_holes(cannyEdges)
+io.imshow(fill_coins)
+io.show()
+
+# Highlight the extremes of the image
+markers = np.zeros_like(image)
+markers[image < 30] = 1
+markers[image > 150] = 2
+
+# Follow the gradient of the image to fill gaps
+segmentation = watershed(elevation_map, markers)
+segmentation = ndi.binary_fill_holes(segmentation - 1)
+
+labeled_coins, _ = ndi.label(segmentation)
+
+io.imshow(labeled_coins)
+io.show()
