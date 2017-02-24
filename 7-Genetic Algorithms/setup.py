6e7d90e05f2aac9cc9c91aaccc1b82fb509da9bf
JP Strydom <jp.strydom@entelect.co.za>
2017-02-24 17:41:26 +0200

    Add create make_warhouses function

--- 7-Genetic Algorithms/setup.py
+++ 7-Genetic Algorithms/setup.py
@@ -36,23 +36,26 @@ for i in xrange(warehouse_num):
     
 
 
-print warehouse_array
+print warehouse_array[0].items
 
-def make_warehouses(num, items, inventory_size):
-    item_size = iy    
-    
+def make_warehouses(num, items, inventory_size):    
     warehouse_num = rand.randint(num[0], num[1])
     warehouse_array = np.empty(warehouse_num, dtype=object)
     
     for i in xrange(warehouse_num):
-        warehouse_size = rand.randint(1, 5)
+        warehouse_size = rand.randint(inventory_size[0], inventory_size[1])
         warehouse_items = np.empty(warehouse_size, dtype=object)
         for j in xrange(warehouse_size):
-            rand_int = rand.randint(0, 4)
+            rand_int = rand.randint(0, np.size(items)-1)
             while items[rand_int] in warehouse_items:
-                rand_int = rand.randint(0, 4)
+                rand_int = rand.randint(0, np.size(items)-1)
             warehouse_items[j] = items[rand_int] 
         
         warehouse = w.Warehouse(i, warehouse_items)
         warehouse_array[i] = warehouse
+    return warehouse_array
+
+warehouse_array_2 = make_warehouses([1, 10], items, [1, 5])
+
+print warehouse_array_2[0].items
     
