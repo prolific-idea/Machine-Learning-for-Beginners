d3f696728fe9553ec8ec67b83fcad8b3e3c6c191
Frikkie Nieuwoudt <frikkie.nieuwoudt@gmail.com>
2017-02-25 18:31:52 +0200

    Improved validation for simulation.

--- 7-Genetic Algorithms/simulation.py
+++ 7-Genetic Algorithms/simulation.py
@@ -8,27 +8,55 @@ class Simulation:
         self.simulation_map = simulation_map
         self.chromosome = chromosome
         self.drones = []
+        self.possible_moves = {}
         for i in range(number_of_drones):
             self.drones.append(d.Drone())
 
-    def check_validity(self):
+    def run(self):
+        self.possible_moves = {"N": self.move_north,
+                               "S": self.move_south,
+                               "W": self.move_west,
+                               "E": self.move_east,
+                               "D": self.drop_item,
+                               "P": self.pickup_item}
         while True:
             gene = self.chromosome.get_move()
             if gene is False:
                 break
-            elif gene.action == 'N':
-                if self.drones[gene.drone_ID].y - 1 < 0:
-                    return False
-                self.drones[gene.drone_ID].move(None, -1)
-            elif gene.action == 'S':
-                if self.drones[gene.drone_ID].y + 1 > self.simulation_map.shape[0]:
-                    return False
-                self.drones[gene.drone_ID].move(None, 1)
-            elif gene.action == 'W':
-                if self.drones[gene.drone_ID].x - 1 < 0:
-                    return False
-                self.drones[gene.drone_ID].move(-1)
-            elif gene.action == 'E':
-                if self.drones[gene.drone_ID].x + 1 > self.simulation_map.shape[1]:
-                    return False
-                self.drones[gene.drone_ID].move(1)
+            elif gene.drone_ID > self.number_of_drones:
+                self.error(gene, "Cannot index drone that does not exist")
+            self.possible_moves[gene.action](gene)
+
+    def move_north(self, gene):
+        if self.drones[gene.drone_ID - 1].yLocation - 1 < 0:
+            self.error(gene, "Map index out of range")
+        self.drones[gene.drone_ID - 1].move(0, -1)
+
+    def move_south(self, gene):
+        if self.drones[gene.drone_ID - 1].yLocation + 1 > self.simulation_map.shape[0]:
+            self.error(gene, "Map index out of range")
+        self.drones[gene.drone_ID - 1].move(0, 1)
+
+    def move_west(self, gene):
+        if self.drones[gene.drone_ID - 1].xLocation - 1 < 0:
+            self.error(gene, "Map index out of range")
+        self.drones[gene.drone_ID - 1].move(-1)
+
+    def move_east(self, gene):
+        if self.drones[gene.drone_ID - 1].xLocation + 1 > self.simulation_map.shape[1]:
+            self.error(gene, "Map index out of range")
+        self.drones[gene.drone_ID - 1].move(1)
+
+    def drop_item(self, gene):
+        if gene.action_parameter is None:
+            self.error(gene, "No item specified to drop")
+
+    def pickup_item(self, gene):
+        if gene.action_parameter is None:
+            self.error(gene, "No item specified to pick-up")
+
+    def error(self, gene, details=""):
+        exit("Invalid gene: droneID: " + str(gene.drone_ID) +
+             ", Action: " + gene.action +
+             ", Action Parameter: " + str(gene.action_parameter) +
+             "\n" + details)
