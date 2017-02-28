3dd40a40d1379918ab3b7b226e540647d22f9f43
Frikkie Nieuwoudt <frikkie.nieuwoudt@entelect.co.za>
2017-02-28 15:40:43 +0200

    Finalized read me

--- 7-Genetic Algorithms/template.py
+++ 7-Genetic Algorithms/template.py
@@ -15,15 +15,15 @@ TODO:
         e.g. gene = Gene(drone_ID, move_str, item_str) {where move_str = 'P' or 'D'; item_str = delivery_items[index]} or
              gene = Gene(drone_ID, move_str) {where move_str = 'N', 'S', 'W', or 'E'}
 
-    Use gene array to construct chromosmes:
+    Use gene array to construct chromosomes:
         e.g. genes = np.array([gene1, gene2,...])
              chromosome = Chromosome(genes)
 
-    Calculate chromosome fittness:
+    Calculate chromosome fitness:
         e.g. fitness = calculator.get_fitness(chromosome)
              fitness array contains [decimal percentage of items delivered, ratio of total distance traveled vs. map area]
 
     Use fitness to generate next generation
 
-    Repeat untill a satisfactory delivery solution has been found
+    Repeat until a satisfactory delivery solution has been found
 """
